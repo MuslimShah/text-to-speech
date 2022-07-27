2b2f7f9b8c6420ac7b78253a49d8da9ee3b011f2
Muslim shah <smuslimshah7@gmailcom>
2022-07-27 06:45:20 -0700

    readme added

---
+++ readme.txt
@@ -0,0 +1,2 @@
+this is a simple node project which takes user input as text
+and convert it into voice using gtts node module
--- routes/all_routes.js
+++ routes/all_routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const routesController = require("../controller/routesController");
 const router = express.Router();
-
 router.get("/convert-text", routesController.getText);
 router.post("/convert", routesController.postText);
 module.exports = router;
